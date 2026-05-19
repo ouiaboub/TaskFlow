2cc85d10a5e3d67e1ae48a62f9141f6b2cdcc1d6
ob-karim <ouiaboubkarim12@gmail.com>
2026-05-19 16:14:06 +0100

    TF-18 #done

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -4,6 +4,7 @@ import LoginForm from '../components/LoginForm';
 export default function LoginPage() {
   return (
 
+
     
     <div className="min-h-screen w-full flex items-center justify-center bg-gray-50 dark:bg-gray-950 bg-gradient-to-br from-indigo-50 via-white to-cyan-50 dark:from-gray-950 dark:via-gray-900 dark:to-indigo-950 relative overflow-hidden">
       {/* Background decorative elements */}
